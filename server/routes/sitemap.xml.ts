c198c8374aca94be64ac301a8a068f47510d120f
Igor Souto <igor_souto@outlook.com>
2025-01-25 20:34:17 +0000

    update sitemap

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -1,23 +1,23 @@
-// import type {
-// 	IndexJobOpportunitiesResponse,
-// } from '~/types/job-opportunities';
+import type {
+	IndexJobOpportunitiesResponse,
+} from '~/types/job-opportunities';
 
 export const WEBSITE_URL = 'https://frontendjobs.app';
 
 export default defineEventHandler(async () => {
-	// const jobs = await fetchJobsFromAPI();
+	const jobs = await fetchJobsFromAPI();
 
-	// const urls = jobs
-	// 	.map(
-	// 		(job: { id: string }) => `
-  //       <url>
-  //         <loc>${WEBSITE_URL}/jobs/${job.id}</loc>
-  //         <lastmod>${new Date().toISOString()}</lastmod>
-  //         <priority>0.8</priority>
-  //       </url>
-  //     `,
-	// 	)
-	// 	.join('');
+	const urls = jobs
+		.map(
+			(job: { id: string }) => `
+        <url>
+          <loc>${WEBSITE_URL}/jobs/${job.id}</loc>
+          <lastmod>${new Date().toISOString()}</lastmod>
+          <priority>0.8</priority>
+        </url>
+      `,
+		)
+		.join('');
 
 	return `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -29,12 +29,12 @@ export default defineEventHandler(async () => {
     </urlset>`;
 });
 
-// async function fetchJobsFromAPI() {
-// 	const config = useRuntimeConfig();
-// 	const response = await $fetch<IndexJobOpportunitiesResponse>('/job_opportunities', {
-// 		method: 'get',
-// 		baseURL: config.public.baseURL,
-// 	});
+async function fetchJobsFromAPI() {
+	const config = useRuntimeConfig();
+	const response = await $fetch<IndexJobOpportunitiesResponse>('/job_opportunities', {
+		method: 'get',
+		baseURL: config.public.baseURL,
+	});
 
-// 	return response.data.map(job => ({ id: job.id }));
-// }
+	return response.data.map(job => ({ id: job.id }));
+}
